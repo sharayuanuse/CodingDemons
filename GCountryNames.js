dc55b254a187f99d3ca45d34346254911f36dd5b
sharayuanuse <114616759+sharayuanuse@users.noreply.github.com>
2022-12-14 10:23:55 +0530

    Update GCountryNames.js

--- GCountryNames.js
+++ GCountryNames.js
@@ -1,29 +1,36 @@
-const GCountryNames= [
+const BTech= [
     {
-      id: 1,
-      Names: 'UK',
-      img: 'https://www.nationsonline.org/gallery/UK/Palace-of-Westminster-Parliament.jpg',
+      Names: 'University of Oxford',
+      img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Funiverlist.com%2Fblog%2Ftop-10-reasons-to-study-at-oxford-university%2F&psig=AOvVaw08BHGE46LMLpJpQz5C7FJj&ust=1671078477181000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiLpcqi-PsCFQAAAAAdAAAAABAR',
     },
     {
-      id: 2,
-      Names: 'USA',
-      img: 'https://www.nationsonline.org/gallery/UK/Palace-of-Westminster-Parliament.jpg',
+      Names: 'University of Cambridge',
+      img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.viator.com%2Fen-IN%2FCambridge-attractions%2FUniversity-of-Cambridge%2Fd22327-a20301&psig=AOvVaw0cu4GKT4Zqa-WNM0IZL3mk&ust=1671078624273000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJi3tJCj-PsCFQAAAAAdAAAAABAE',
     },
     {
-      id: 3,
-      Names: 'Germany',
-      img: 'https://www.worldatlas.com/r/w1200/upload/21/11/9e/berlin-germany-country-government.jpg',
+      Names: 'University of Manchester',
+      img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hotcoursesabroad.com%2Fstudy%2Fuk%2Fschool-college-university%2Finto-manchester-university-of-manchester%2F3917%2Finternational.html&psig=AOvVaw2KmEx5EAnzRvyoKn-B9i4N&ust=1671079287901000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNi_-syl-PsCFQAAAAAdAAAAABAF',
     },
     {
-      id: 4,
-      Names: 'Canada',
-      img: 'https://static.india.com/wp-content/uploads/2018/08/Canada-1.jpg',
+      Names: 'University of Birmingham',
+      img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.architectsjournal.co.uk%2Fcompetitions%2Funiversity-of-birmingham-2045-masterplan&psig=AOvVaw2d29Pb9rh9uSzhqfwl4-Jr&ust=1671079328899000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNiKpuCl-PsCFQAAAAAdAAAAABAZ',
     },
     {
-      id: 5,
-      Names: 'Australia',
-      img: 'https://i.insider.com/5f3424f2988ee31668198a09?width=1136&format=jpeg',
+      Names: 'University College London',
+      img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wikidata.org%2Fwiki%2FQ193196&psig=AOvVaw0jMZ413Q8ihg5XjItMPnQT&ust=1671079474666000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPif_qWm-PsCFQAAAAAdAAAAABAE',
+    },
+    {
+      Names: 'Imperial College London',
+      img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imperial.ac.uk%2F&psig=AOvVaw02pO3X4dGaMvAuCa-Y6z7G&ust=1671079550200000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCODy9smm-PsCFQAAAAAdAAAAABAE',
+    },
+    {
+      Names: 'University of Edinburgh ',
+      img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ed.ac.uk%2Fstudying%2Fundergraduate%2Fedinburgh%2Finvest&psig=AOvVaw0gsDZBSzpORx3C033BJXyd&ust=1671079605654000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKjqoOSm-PsCFQAAAAAdAAAAABAJ',
+    },
+    {
+      Names: 'King’s College London',
+      img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Findiaeducationdiary.in%2Fstudents-of-kings-college-london-help-londons-west-end-thrive%2F&psig=AOvVaw3LU3tjTxmwCyfgJMJOqJAf&ust=1671079675849000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKie_YWn-PsCFQAAAAAdAAAAABAE',
     },
   ];
 
-export default GCountryNames;
+export default BTech;
